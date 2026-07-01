ba4f635c7a8808927f7c46cf4b67d217d35f372e
Deepak Maan <dipumaan2002@gmail.com>
2026-07-01 11:37:07 +0530

    Update index.ts

--- src/components/providers/index.ts
+++ src/components/providers/index.ts
@@ -1,5 +1,10 @@
 export { AppProviders } from "./AppProviders";
 export { ThemeProvider, useTheme } from "./ThemeProvider";
+export {
+  LayoutProvider,
+  useLayout,
+  useLayoutMode,
+} from "./LayoutProvider";
 export { QueryProvider } from "./QueryProvider";
 export { ToastProvider, useToast } from "./ToastProvider";
 export type { ToastInput, ToastTone } from "./ToastProvider";
