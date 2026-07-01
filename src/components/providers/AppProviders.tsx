1f4e07e8b47a229abbbb5c0c363ed71c12c03427
Deepak Maan <dipumaan2002@gmail.com>
2026-07-01 11:36:31 +0530

    Update AppProviders.tsx

--- src/components/providers/AppProviders.tsx
+++ src/components/providers/AppProviders.tsx
@@ -2,15 +2,25 @@
 
 import type { ReactNode } from "react";
 import { ThemeProvider } from "./ThemeProvider";
+import { LayoutProvider } from "./LayoutProvider";
+import type { LayoutPref } from "@/lib/layout/constants";
 import { QueryProvider } from "./QueryProvider";
 import { ToastProvider } from "./ToastProvider";
 
-export function AppProviders({ children }: { children: ReactNode }) {
+export function AppProviders({
+  children,
+  initialLayoutPref,
+}: {
+  children: ReactNode;
+  initialLayoutPref?: LayoutPref;
+}) {
   return (
     <ThemeProvider>
-      <QueryProvider>
-        <ToastProvider>{children}</ToastProvider>
-      </QueryProvider>
+      <LayoutProvider initialPref={initialLayoutPref}>
+        <QueryProvider>
+          <ToastProvider>{children}</ToastProvider>
+        </QueryProvider>
+      </LayoutProvider>
     </ThemeProvider>
   );
 }
